6097d7a4d0a6c5f2d4cdf5c09a02c9d854994789
Michael Quarcoo <michaelquarcoo04@gmail.com>
2021-05-15 20:16:49 +0000

    Using Passport to add Cookies

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
-mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -18,12 +17,31 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+app.use(session({
+    secret: "It's supposed to be a secret!",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
     res.render("home");
@@ -37,57 +55,58 @@ app.get("/register", function(req, res){
     res.render("register");
 });
 
-app.post("/register", function(req, res){
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save(function(err){
-            if(!err){
-                res.render("secrets");
-            }else{
-                console.log(err);
-            };
-        });
-    });
-    
-});
-
-app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({email: username}, function(err, foundUser){
-        if(!err){
-            if(foundUser){
-                bcrypt.compare(password, foundUser.password, function(err, result) {
-                    if(result === true){
-                        res.render("secrets");
-                    }else{
-                        res.send("Incorrect password!")
-                    }
-                });     
-            }else{
-                res.send("Username does not exist!");
-            }
-        }else {
-            console.log(err);
-        } 
-    });
-});
-
-// app.get("/secrets", function(req, res){
-//     res.render("secrets");
-// });
-
 app.get("/submit", function(req, res){
     res.render("submit");
 });
 
 app.get("/logout", function(req, res){
-    res.render("home");
-})
+    req.logout();
+    res.redirect("/");
+});
+
+app.get("/secrets", function(req, res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+
+app.post("/register", function(req, res){
+
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(!err){
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }else{
+            console.log(err);
+            res.redirect("/register");
+        }
+    });
+    
+});
+
+app.post("/login", function(req, res){
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(!err){
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            });
+        }else{
+            console.log(err);
+            res.redirect("/register");
+        }
+    });
+    
+});
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
   });
